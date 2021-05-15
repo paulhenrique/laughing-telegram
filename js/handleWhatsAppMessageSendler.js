a26cf308b97245e4ec35da043be5cb0b1810eff2
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 14:22:17 -0300

    Adicionando campo de nome do usuário

--- js/handleWhatsAppMessageSendler.js
+++ js/handleWhatsAppMessageSendler.js
@@ -2,10 +2,12 @@ document.querySelector("#formDeContato").addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.querySelector("#emailContainer").value;
   const message = document.querySelector("#messageContainer").value;
+  const name = document.querySelector("#nomeContainer").value;
+
   const number = '5515998317100';
   const address = 'https://wa.me';
 
-  const uriToSend = `${address}/${number}?text=Olá, meu e-mail é ${email}, estou entrando em contato através do site do Hello Yotta.\n Essa é a minha mensagem: ${message}`;
+  const uriToSend = `${address}/${number}?text=Olá, meu nome é ${name} e meu e-mail é ${email}. Estou entrando em contato através do site do Hello Yotta.\n Essa é a minha mensagem: ${message}`;
 
   location.href = uriToSend;
 });
