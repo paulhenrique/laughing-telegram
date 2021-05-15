ab7198ec5c25b4ec3c8c7daf522e9fc39a15331e
Paulo Henrique <phvcandido@gmail.com>
2021-05-15 12:35:29 -0300

    Adicionando manipulação do envio de mensagens

---
+++ js/handleWhatsAppMessageSendler.js
@@ -0,0 +1,11 @@
+document.querySelector("#formDeContato").addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#emailContainer").value;
+  const message = document.querySelector("#messageContainer").value;
+  const number = '551599831-7100';
+  const address = 'https://wa.me';
+
+  const uriToSend = `${address}/${number}?text=Olá, meu e-mail é ${email}, estou entrando em contato através do site do Hello Yotta.\n Essa é a minha mensagem: ${message}`;
+
+  location.href = uriToSend;
+});
