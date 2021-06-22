bb4a6b63477c6761c69ea250ab8ecf93c2abaff8
Paulo Henrique <phvcandido@gmail.com>
2021-06-22 19:02:38 -0300

    Atualizando número de whatsapp em contato

--- js/handleWhatsAppMessageSendler.js
+++ js/handleWhatsAppMessageSendler.js
@@ -4,7 +4,7 @@ document.querySelector("#formDeContato").addEventListener('submit', (e) => {
   const message = document.querySelector("#messageContainer").value;
   const name = document.querySelector("#nomeContainer").value;
 
-  const number = '5515998317100';
+  const number = '5515997088134';
   const address = 'https://wa.me';
 
   const uriToSend = `${address}/${number}?text=Olá, meu nome é ${name} e meu e-mail é ${email}. Estou entrando em contato através do site do Hello Yotta.\n Essa é a minha mensagem: ${message}`;
